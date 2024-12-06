6d2de592240ef19cef90935f017f8e3ac68b292a
TitleRKD <sirawutpa@gmail.com>
2024-12-06 17:23:23 +0700

    lab8_3 finished

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,5 +1,5 @@
 /*
-"Enter your age: "
+c
 "Enter your height: "
 "Enter your bounty: "
 "Your character = "
@@ -11,3 +11,34 @@
 "Brook"
 "Jinbe"
 */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+string name;
+int age;
+int height;
+int Bounty;
+cout << "Enter your age: ";
+cin >> age;
+if(age <= 25){
+cout << "Enter your height: ";
+cin >> height;
+if(height < 100) name = "Chopper";
+else if(height < 180) name = "Usopp";
+else {cout << "Enter your bounty: ";
+cin >> Bounty;
+if (Bounty > 11e8)
+name = "Zoro";
+else name = "Sanji";}
+}else if(age <= 60){ cout << "Enter your bounty: ";
+cin >> Bounty;
+if(Bounty > 5e8) name = "Jinbe";
+else name = "Franky";}
+else name = "Brook";
+
+cout << "Your character = " << name ;
+
+return 0;
+}
